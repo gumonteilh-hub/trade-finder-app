52a109f5c24448d7a43c6116434890f4d41d562f
guigui <dev.crypto.guigui@gmail.com>
2022-12-08 15:36:00 +0100

    fix poketype layout + responsive

--- src/components/PokemonInfo.tsx
+++ src/components/PokemonInfo.tsx
@@ -13,11 +13,13 @@ export function PokemonInfo({ pokemon }: Props) {
             <div className='flex flex-col m-auto space-y-5'>
                 <p>Nom : <strong>{pokemon.nomFrancais}</strong></p>
                 <p>Numéro pokedex national : <strong>#{pokemon.nationalPokedexNumber}</strong></p>
-                <PokeType poketype={pokemon.type1}></PokeType>
-                <PokeType poketype={pokemon.type2}></PokeType>
+                <div className="flex flex-col lg:flex-row">
+                    <PokeType poketype={pokemon.type1}></PokeType>
+                    <PokeType poketype={pokemon.type2}></PokeType>
+                </div>
             </div>
 
-            <img src={pokemon.imageUrl} alt="" />
+            <img src={pokemon.imageUrl} alt={pokemon.nomFrancais} />
 
         </div>
     </>
