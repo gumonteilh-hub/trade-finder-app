f9d69c062a353ffa86e7b7c362834741e2a144a1
guigui <guillaumemont@outlook.fr>
2023-02-01 23:45:20 +0100

    add responsive handling

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,37 @@ import { PokemonList } from './components/PokemonList'
 import { NewOffer } from './components/NewOffer'
 import { Header } from './components/Header'
 import { OfferList } from './components/OfferList'
+import useWindowDimensions from './utils'
+import workInProgress from "./assets/work-in-progress.png"
 
 function App() {
-  return (
-    <>
-      <Header></Header>
+  const { height, width } = useWindowDimensions();
 
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/pokemons' element={<PokemonList />} />
-        <Route path='/newOffer/:id' element={<NewOffer />} />
-        <Route path='/newOffer/' element={<NewOffer />} />
-        <Route path='/offer/:id' element={<OfferList />} />
-        <Route path='/offer/' element={<OfferList />} />
-      </Routes>
+  return (
+
+
+    <>
+      {width > 800
+        ? <>
+          <Header></Header>
+
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/pokemons' element={<PokemonList />} />
+            <Route path='/newOffer/:id' element={<NewOffer />} />
+            <Route path='/newOffer/' element={<NewOffer />} />
+            <Route path='/offer/:id' element={<OfferList />} />
+            <Route path='/offer/' element={<OfferList />} />
+            <Route path='*' element={<Home />} />
+          </Routes>
+        </>
+        :
+        <div className='mt-10 mx-[25%]'>Je n'ai malheureusement pas encore rendu le site responsive / adapté aux mobiles.<br />
+          Je vous invite à réessayer sur un PC ou à dézoomer.
+          <img src={workInProgress} className="m-auto pt-5 w-20"></img>
+        </div>
+
+      }
     </>
   )
 }
--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,5 @@
+import { useState, useEffect } from 'react';
+
 export const validateEmail = (email: String) => {
     return String(email)
         .toLowerCase()
@@ -5,3 +7,28 @@ export const validateEmail = (email: String) => {
             /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
         );
 };
+
+
+
+function getWindowDimensions() {
+  const { innerWidth: width, innerHeight: height } = window;
+  return {
+    width,
+    height
+  };
+}
+
+export default function useWindowDimensions() {
+  const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
+
+  useEffect(() => {
+    function handleResize() {
+      setWindowDimensions(getWindowDimensions());
+    }
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  return windowDimensions;
+}
